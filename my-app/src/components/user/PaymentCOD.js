a44af97cd802d349d21b3f9671fdbc6b4648dfac
Rick <trongtri.1975s@gmail.com>
2025-04-23 19:20:33 +0700

    Update navigate payment

--- my-app/src/components/user/PaymentCOD.js
+++ my-app/src/components/user/PaymentCOD.js
@@ -54,7 +54,7 @@ const PaymentCOD = () => {
                                         Về trang chủ
                                     </Link>
                                     <Link
-                                        to="/orders"
+                                        to="/account/ordered"
                                         className="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                     >
                                         Xem đơn hàng
